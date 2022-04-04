e46e8b2c268d1235ea4f424647890dc5897ea543
Gourav <grvrawat999@gmail.com>
2022-04-04 13:41:10 +0530

    multer to handle multipart body

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const badWords = require("./badwords.json");
+const multer = require("multer");
+const upload = multer();
 const { Deepgram } = require("@deepgram/sdk");
 
 dotenv.config();
@@ -24,39 +26,43 @@ function findCommonElement(array1, array2) {
   return false;
 }
 
-router.post("/response", async (req, res) => {
+router.post("/response", upload.none(), async (req, res) => {
   const transcriptionArray = [];
 
+  const formData = req.body;
+  console.log(formData);
+
   // Getting request
-  const audioStream = req.body.audioURL;
-  const mimeType = req.body.mimeType;
+  const audioStream = req;
+  const mimeType = "audio/webm";
 
-  const bufferData = Buffer.from(audioStream, "binary"); // Buffer data
-  const bufferString = bufferData.toString("hex").match(/../g).join(" ");
+  res.send("Hello world");
+  // const bufferData = Buffer.from(audioStream, "binary"); // Buffer data
+  // const bufferString = bufferData.toString("hex").match(/../g).join(" ");
 
-  console.log(bufferData);
+  // console.log(bufferData);
 
-  deepgram.transcription
-    .preRecorded(
-      { buffer: bufferData, mimetype: mimeType },
-      { punctuate: true, language: "en-GB" }
-    )
-    .then((transcription) => {
-      const transcriptionObject =
-        transcription.results.channels[0].alternatives[0].words;
-      transcriptionObject.map((wordData) => {
-        transcriptionArray.push(wordData.word);
-      });
-      res.send(transcription);
-      if (findCommonElement(transcriptionArray, badWordsArray)) {
-        console.log("Not safe!");
-      } else {
-        console.log("Safe!");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // deepgram.transcription
+  //   .preRecorded(
+  //     { buffer: bufferData, mimetype: mimeType },
+  //     { punctuate: true, language: "en-GB" }
+  //   )
+  //   .then((transcription) => {
+  //     const transcriptionObject =
+  //       transcription.results.channels[0].alternatives[0].words;
+  //     transcriptionObject.map((wordData) => {
+  //       transcriptionArray.push(wordData.word);
+  //     });
+  //     res.send(transcription);
+  //     if (findCommonElement(transcriptionArray, badWordsArray)) {
+  //       console.log("Not safe!");
+  //     } else {
+  //       console.log("Safe!");
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
 module.exports = router;
